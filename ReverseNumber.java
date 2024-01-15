ef2798e6aed2b1ac3380a3b293ddcf345d9a8522
vt <writetovt7@gmail.com>
2024-01-16 00:48:20 +0530

    ReverseNunmber

---
+++ ReverseNumber.java
@@ -0,0 +1,13 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345, reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed number: " + reversed);  // Output: 54321
+    }
+}
